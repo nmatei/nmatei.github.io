34629978c1dde01ceba68dfd818c815f9faca62a
Nicolae Matei <nmatei@sdl.com>
2020-11-12 21:08:20 +0200

    use hideAllPages function

--- functions.js
+++ functions.js
@@ -9,30 +9,29 @@ function hide(id) {
     }
 }
 
-function showHome() {
+function hideAllPages() {
+    hide("home");
     hide("skills");
     hide("projects");
     hide("languages");
+}
+
+function showHome() {
+    hideAllPages();
     document.getElementById('home').style.display = '';
 }
 
 function showSkills() {
-    hide("home");
-    hide("projects");
-    hide("languages");
+    hideAllPages();
     document.getElementById('skills').style.display = '';
 }
 
 function showProjects() {
-    hide("home");
-    hide("skills");
-    hide("languages");
+    hideAllPages();
     document.getElementById("projects").style.display = "";
 }
 
 function showLanguages() {
-    hide("home");
-    hide("skills");
-    hide("projects");
+    hideAllPages();
     document.getElementById("languages").style.display = "";
 }
