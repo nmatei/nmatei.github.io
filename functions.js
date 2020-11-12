0d180fa37a69333aa9833f96845da3bf011742a3
Nicolae Matei <nmatei@sdl.com>
2020-11-12 21:51:38 +0200

    hide all pages by page cls

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 function hide(id) {
     //document.getElementById(id).style.display = "none";
     var el = document.getElementById(id);
-    console.info("hide:" + id, el);
+    //console.info("hide:" + id, el);
     if (el) {
         el.style.display = "none";
     } else {
@@ -10,10 +10,14 @@ function hide(id) {
 }
 
 function hideAllPages() {
-    hide("home");
-    hide("skills");
-    hide("projects");
-    hide("languages");
+    var pages = document.querySelectorAll(".page");
+    //for(initializare; conditie; post execute)
+    for(var i = 0; i < pages.length; i++) {
+        var page = pages[i];
+        var id = page.id;
+        console.info("i=", i, id, page);
+        hide(id);
+    }
 }
 
 function showHome() {
