2484d35fe71088cb86466c3501a3ace3e20a37d0
Nicolae Matei <nmatei@sdl.com>
2020-11-23 21:12:03 +0200

    add showSkills fn

--- functions.js
+++ functions.js
@@ -37,16 +37,21 @@ initMenu();
 
 showPage(activePage);
 
-var skills = [
+var allSkills = [
     { name: "HTML", endorsements: 15 },
     { name: "CSS", endorsements: 5 },
     { name: "JS", endorsements: 22 }
 ];
-var skillsLi = skills.map(function(skill){
-    var endorsements = ` <span>&middot; ${skill.endorsements}</span>`;
-    return "<li>" + skill.name + endorsements + "</li>";
-});
 
-// TODO add "favorite" skill
-var ul = document.querySelector("#skills ul");
-ul.innerHTML = skillsLi.join("");
+function showSkills(skills) {
+    var skillsLi = skills.map(function(skill){
+        var endorsements = ` <span>&middot; ${skill.endorsements}</span>`;
+        return "<li>" + skill.name + endorsements + "</li>";
+    });
+    
+    // TODO add "favorite" skill
+    var ul = document.querySelector("#skills ul");
+    ul.innerHTML = skillsLi.join("");
+}
+
+showSkills(allSkills);
