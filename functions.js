e1804b78e7e17f3ea2fba52201eb6834907f7b33
Nicolae Matei <nmatei@sdl.com>
2020-12-07 21:05:15 +0200

    add const and let

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
-var activePage = "skills";
+let activePage = "skills";
 
 function hide(id) {
-    var el = document.getElementById(id);
+    const el = document.getElementById(id);
     if (el) {
         el.style.display = "none";
     } else {
@@ -11,23 +11,23 @@ function hide(id) {
 
 function hidePreviousPage () {
     hide(activePage);
-    var link = document.querySelector(`#top-menu-bar a[data-page="${activePage}"]`);
+    const link = document.querySelector(`#top-menu-bar a[data-page="${activePage}"]`);
     link.classList.remove("active");
 }
 
 function showPage(pageId) {
     hidePreviousPage();
     document.getElementById(pageId).style.display = "";
-    var link = document.querySelector(`#top-menu-bar a[data-page="${pageId}"]`);
+    const link = document.querySelector(`#top-menu-bar a[data-page="${pageId}"]`);
     link.classList.add("active");
     activePage = pageId;
 }
 
 function initMenu() {
     document.addEventListener("click", function(e){
-        var link = e.target;
+        const link = e.target;
         if (link.matches("#top-menu-bar a")) {
-            var id = link.getAttribute("data-page");
+            const id = link.getAttribute("data-page");
             showPage(id);
         }
     })
@@ -38,16 +38,15 @@ initMenu();
 showPage(activePage);
 
 function getHTMLSkills(skills) {
-    var skillsLi = skills.map(function(skill){
+    return skills.map(function(skill){
         return `<li class="${skill.endorsements > 9 ? "favorite" : ""}">
             ${skill.name} <span>&middot; ${skill.endorsements}</span>
         </li>`;
-    });
-    return skillsLi.join("");
+    }).join("");
 }
 
 function showSkills(skills) {
-    var ul = document.querySelector("#skills ul");
+    const ul = document.querySelector("#skills ul");
     ul.innerHTML = getHTMLSkills(skills);
 }
 
@@ -60,5 +59,3 @@ fetch("data/skills.json").then(function(r) {
 
     showSkills(allSkills);
 });
-
-// TODO intro in es6
