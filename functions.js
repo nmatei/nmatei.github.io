6274d5c47de1aa76a0d5cabb65f6327d3e48ba63
Nicolae Matei <nmatei@sdl.com>
2021-06-24 21:12:57 +0300

    make main menu functional

--- functions.js
+++ functions.js
@@ -12,7 +12,30 @@ console.info(getInfo("Nick " + "Matei"));
 // var wellcome = getInfo(myName);
 // console.info(wellcome);
 
+function showHome() {
+  document.getElementById('skills').style.display = 'none';
+  document.getElementById('projects').style.display = 'none';
+  document.getElementById('languages').style.display = 'none';
+  document.getElementById('home').style.display = 'block';
+}
+
 function showSkills() {
   document.getElementById('home').style.display = 'none';
+  document.getElementById('projects').style.display = 'none';
+  document.getElementById('languages').style.display = 'none';
   document.getElementById('skills').style.display = 'block';
+}
+
+function showProjects() {
+  document.getElementById('home').style.display = 'none';
+  document.getElementById('skills').style.display = 'none';
+  document.getElementById('languages').style.display = 'none';
+  document.getElementById('projects').style.display = 'block';
+}
+
+function showLanguages() {
+  document.getElementById('home').style.display = 'none';
+  document.getElementById('skills').style.display = 'none';
+  document.getElementById('projects').style.display = 'none';
+  document.getElementById('languages').style.display = 'block';
 }
