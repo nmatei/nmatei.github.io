9f0019a861b7319a6c8939943755818105e37f51
Nicolae Matei <nmatei@sdl.com>
2021-06-24 21:20:57 +0300

    cleanup not used code

--- functions.js
+++ functions.js
@@ -1,17 +1,3 @@
-console.debug('inside functions.js');
-
-function getInfo(name) {
-  var mess = "Salut " + name + ", bine ai venit!";
-  console.warn('inside get info', mess);
-  return mess;
-}
-
-console.info(getInfo("Nick " + "Matei"));
-
-// var myName = "Nick " + "Matei";
-// var wellcome = getInfo(myName);
-// console.info(wellcome);
-
 function showHome() {
   document.getElementById('skills').style.display = 'none';
   document.getElementById('projects').style.display = 'none';
