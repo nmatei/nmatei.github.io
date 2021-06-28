80428cd2f739398681a17c5f0410b5bec10b3b8a
Nicolae Matei <nmatei@sdl.com>
2021-06-28 19:24:46 +0300

    improve header info (css)

--- functions.js
+++ functions.js
@@ -3,30 +3,55 @@ function hidePage(id) {
   document.getElementById(id).style.display = 'none';
 }
 
+function highlight(el) {
+  el.style.borderColor = '#00df00';
+  setTimeout(function () {
+    el.style.borderColor = '#55df44';
+  }, 100);
+  setTimeout(function () {
+    el.style.borderColor = '#99df88';
+  }, 200);
+  setTimeout(function () {
+    el.style.borderColor = '#e0dfdc';
+  }, 300);
+}
+
+function showPage(id) {
+  console.info('show page', id);
+  var page = document.getElementById(id);
+  console.info('show', page);
+  if (page) {
+    page.style.display = 'block';
+    highlight(page);
+  } else {
+    console.warn("pagina cu id-ul %o nu exista", id);
+  }
+}
+
 function showHome() {
   hidePage('skills');
   hidePage('projects');
   hidePage('languages');
-  document.getElementById('home').style.display = 'block';
+  showPage('home');
 }
 
 function showSkills() {
   hidePage('home');
   hidePage('projects');
   hidePage('languages');
-  document.getElementById('skills').style.display = 'block';
+  showPage('skills');
 }
 
 function showProjects() {
   hidePage('home');
   hidePage('skills');
   hidePage('languages');
-  document.getElementById('projects').style.display = 'block';
+  showPage('projects');
 }
 
 function showLanguages() {
   hidePage('home');
   hidePage('skills');
   hidePage('projects');
-  document.getElementById('languages').style.display = 'block';
+  showPage('languages');
 }
