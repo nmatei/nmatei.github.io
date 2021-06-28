7941da192e673e34d9df102cae8ba9119dd9a901
Nicolae Matei <nmatei@sdl.com>
2021-06-28 20:37:20 +0300

    use "for" instead of "while"

--- functions.js
+++ functions.js
@@ -32,10 +32,9 @@ function hideAllPages() {
   var pageIds = [
     'home', 'skills', 'projects', 'languages'
   ];
-  var i = 0;
 
-  while(i < pageIds.length) {
-    hidePage(pageIds[i++]);
+  for (var i = 0; i < pageIds.length; i++) {
+    hidePage(pageIds[i]);
   }
 }
 
