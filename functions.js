09bec01d47ceeeab33b8f4e80c0c7f04027032fd
Matei Nicolae <nmatei@sdl.com>
2022-07-16 18:04:06 +0300

    change SDL to RWS + prettier

--- functions.js
+++ functions.js
@@ -1,36 +1,36 @@
 let activePage = "home";
 
 function hide(id) {
-    const el = document.getElementById(id);
-    if (el) {
-        el.style.display = "none";
-    } else {
-        console.error("elementul nu exista");
-    }
+  const el = document.getElementById(id);
+  if (el) {
+    el.style.display = "none";
+  } else {
+    console.error("element you are searching does not exist... check your selector");
+  }
 }
 
-function hidePreviousPage () {
-    hide(activePage);
-    const link = document.querySelector(`#top-menu-bar a[data-page="${activePage}"]`);
-    link.classList.remove("active");
+function hidePreviousPage() {
+  hide(activePage);
+  const link = document.querySelector(`#top-menu-bar a[data-page="${activePage}"]`);
+  link.classList.remove("active");
 }
 
 function showPage(pageId) {
-    hidePreviousPage();
-    document.getElementById(pageId).style.display = "";
-    const link = document.querySelector(`#top-menu-bar a[data-page="${pageId}"]`);
-    link.classList.add("active");
-    activePage = pageId;
+  hidePreviousPage();
+  document.getElementById(pageId).style.display = "";
+  const link = document.querySelector(`#top-menu-bar a[data-page="${pageId}"]`);
+  link.classList.add("active");
+  activePage = pageId;
 }
 
 function initMenu() {
-    document.addEventListener("click", e => {
-        const link = e.target;
-        if (link.matches("#top-menu-bar a")) {
-            const id = link.getAttribute("data-page");
-            showPage(id);
-        }
-    })
+  document.addEventListener("click", e => {
+    const link = e.target;
+    if (link.matches("#top-menu-bar a")) {
+      const id = link.getAttribute("data-page");
+      showPage(id);
+    }
+  });
 }
 
 initMenu();
@@ -38,24 +38,25 @@ initMenu();
 showPage(activePage);
 
 function getHTMLSkills(skills) {
-    return skills.map(skill => 
+  return skills
+    .map(
+      skill =>
         `<li class="${skill.endorsements > 9 ? "favorite" : ""}">
             ${skill.name} <span>&middot; ${skill.endorsements}</span>
         </li>`
-    ).join("");
+    )
+    .join("");
 }
 
 function showSkills(skills) {
-    const ul = document.querySelector("#skills ul");
-    ul.innerHTML = getHTMLSkills(skills);
+  const ul = document.querySelector("#skills ul");
+  ul.innerHTML = getHTMLSkills(skills);
 }
 
 fetch("data/skills.json")
-    .then(r => r.json())
-    .then((allSkills) => {
-        allSkills.sort((s1, s2) => s2.endorsements - s1.endorsements);
+  .then(r => r.json())
+  .then(allSkills => {
+    allSkills.sort((s1, s2) => s2.endorsements - s1.endorsements);
 
-        showSkills(allSkills);
-    });
-
-
+    showSkills(allSkills);
+  });
