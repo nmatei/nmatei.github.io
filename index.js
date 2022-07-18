1e1cfb45f316528163b5e496faa7f4e87ffd0e28
Matei Nicolae <nmatei@sdl.com>
2022-07-18 20:41:01 +0300

    add hideAllPages

--- index.js
+++ index.js
@@ -27,31 +27,30 @@ function showPage(id) {
   document.getElementById(id).style.display = "block";
 }
 
-function showHomePage() {
+function hideAllPages() {
+  hidePage("home");
   hidePage("skills");
   hidePage("projects");
   hidePage("languages");
+}
+
+function showHomePage() {
+  hideAllPages();
   showPage("home");
 }
 
 function showSkillsPage() {
-  hidePage("home");
-  hidePage("projects");
-  hidePage("languages");
+  hideAllPages();
   showPage("skills");
 }
 
 function showProjectsPage() {
-  hidePage("home");
-  hidePage("skills");
-  hidePage("languages");
+  hideAllPages();
   showPage("projects");
 }
 
 function showLanguagesPage() {
-  hidePage("home");
-  hidePage("skills");
-  hidePage("projects");
+  hideAllPages();
   showPage("languages");
 }
 
