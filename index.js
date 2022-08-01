54f7b1dd09a3b40c580f8d870afc852065fe9915
Matei Nicolae <nmatei@sdl.com>
2022-08-01 19:50:23 +0300

    display skills from js

--- index.js
+++ index.js
@@ -53,3 +53,23 @@ function initEvents() {
 
 showPage(activePage);
 initEvents();
+
+function displaySkills() {
+  console.info("display skills");
+
+  // initializare ; conditie ; post exec
+  for (var i = 1; i < 3; i++) {
+    console.info(`${i} * 5 = ${i * 5}`);
+  }
+
+  var ul = document.querySelector("#skills ul");
+  var skills = ["js", "html", "css"];
+  console.info(ul);
+  for (var i = 0; i < skills.length; i++) {
+    // ul.innerHTML = `<li>${skills[i]}</li>` + ul.innerHTML;
+    // ul.innerHTML = ul.innerHTML + `<li>${skills[i]}</li>`;
+    ul.innerHTML += `<li>${skills[i]}</li>`;
+  }
+}
+
+displaySkills();
