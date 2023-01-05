07253256e364222dce4c87e9e2d8ec2b6e6c4896
Matei Nicolae <nmatei@sdl.com>
2023-01-05 21:28:20 +0200

    add hideAllPages function

--- index.js
+++ index.js
@@ -10,28 +10,27 @@ for (var i = 0; i < skills.length; i++) {
 
 skillsEl.innerHTML = skillsHTML;
 
-function displayHome() {
+function hideAllPages() {
+  document.getElementById("home").style.display = "none";
   document.getElementById("skills").style.display = "none";
   document.getElementById("languages").style.display = "none";
   document.getElementById("projects").style.display = "none";
+}
+
+function displayHome() {
+  hideAllPages();
   document.getElementById("home").style.display = "block";
 }
 function displaySkills() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("languages").style.display = "none";
-  document.getElementById("projects").style.display = "none";
+  hideAllPages();
   document.getElementById("skills").style.display = "block";
 }
 function displayLanguages() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("projects").style.display = "none";
+  hideAllPages();
   document.getElementById("languages").style.display = "block";
 }
 function displayProjects() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("languages").style.display = "none";
+  hideAllPages();
   document.getElementById("projects").style.display = "block";
 }
 
