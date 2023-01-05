fbee2651e9338a01353e355bc57a8eee861e815a
Matei Nicolae <nmatei@sdl.com>
2023-01-05 21:08:05 +0200

    display pages with js

--- index.js
+++ index.js
@@ -10,4 +10,29 @@ for (var i = 0; i < skills.length; i++) {
 
 skillsEl.innerHTML = skillsHTML;
 
-document.getElementById("home").style.display = "block";
+function displayHome() {
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("home").style.display = "block";
+}
+function displaySkills() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("skills").style.display = "block";
+}
+function displayLanguages() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "block";
+}
+function displayProjects() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("projects").style.display = "block";
+}
+
+displayHome();
