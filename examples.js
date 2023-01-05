b4d84227c60a89a63d0299eba3d4d51ffd8ca1e1
Matei Nicolae <nmatei@sdl.com>
2023-01-05 19:12:29 +0200

    extract inline js into examples.js

---
+++ examples.js
@@ -0,0 +1,27 @@
+// asa vreau eu!
+console.info("page loaded");
+console.debug(2 * 3 + 1);
+2 * (3 + 1);
+
+var employed = true;
+console.error(employed);
+
+var age = 24; // number
+
+var user = "Matei";
+console.info(user);
+user = user + " Nicolae";
+console.info(user);
+
+var skills = ["html", "css", "js"];
+
+var dan = {
+  age: 26,
+  employed: true,
+};
+var gulia = {
+  age: 25,
+  employed: true,
+};
+
+var jobTitle = document.getElementById("job-title");
