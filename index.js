ba3ddf2feab28b2f0d9df4bf52cf04aef9d8ab42
Matei Nicolae <nmatei@sdl.com>
2023-01-12 19:03:11 +0200

    add hide function

--- index.js
+++ index.js
@@ -11,14 +11,17 @@ for (var i = 0; i < skills.length; i++) {
 skillsEl.innerHTML = skillsHTML;
 
 function hideAllPages() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("languages").style.display = "none";
-  document.getElementById("projects").style.display = "none";
+  hide("home");
+  hide("skills");
+  hide("languages");
+  hide("projects");
 }
 
 function show(id) {
   document.getElementById(id).style.display = "block";
+}
+function hide(id) {
+  document.getElementById(id).style.display = "none";
 }
 
 function displayHome() {
