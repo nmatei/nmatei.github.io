b98e5819728e37f0494bb507102b3405836b643f
Matei Nicolae <nmatei@sdl.com>
2023-01-12 20:12:06 +0200

    remove custom functions for display pages

--- index.js
+++ index.js
@@ -25,13 +25,6 @@ function hide(id) {
   document.getElementById(id).style.display = "none";
 }
 
-function displayLanguages() {
-  displayPage("languages");
-}
-function displayProjects() {
-  displayPage("projects");
-}
-
 function displayPage(id) {
   hideAllPages();
   show(id);
