6e93fa75aadf84e481e711d2b4735b1fc7985a51
Matei Nicolae <nmatei@sdl.com>
2023-04-03 17:32:06 +0300

    implement show pages functions

--- index.js
+++ index.js
@@ -4,7 +4,29 @@ var homePage = document.getElementById(activePage);
 homePage.style.display = "block";
 
 function showSkills() {
-  console.warn("skills");
   document.getElementById("home").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
   document.getElementById("skills").style.display = "block";
+}
+
+function showHome() {
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("home").style.display = "block";
+}
+
+function showProjects() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("projects").style.display = "block";
+}
+
+function showLanguages() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "block";
 }
