2c557877c819cc0ab1d28684c85d6f9d8591821e
Matei Nicolae <nmatei@sdl.com>
2023-04-08 21:41:57 +0300

    4.24 functions

--- examples.js
+++ examples.js
@@ -1,35 +1,43 @@
-console.log(true);
-console.info("page loaded");
-console.debug(5 - 2);
-console.warn(10 / 2);
-console.error("hello " + "there");
+function variablesExample() {
+  console.log(true);
+  console.info("page loaded");
+  console.debug(5 - 2);
+  console.warn(10 / 2);
+  console.error("hello " + "there");
 
-var employed = false;
-var age = 31 + 2;
-var name = `Nick's "Matei"`;
-console.info("I'm employed", employed);
-console.info(typeof employed);
-console.warn("age", age, typeof age);
-console.info("My name is", name, typeof name);
+  var employed = false;
+  var age = 31 + 2;
+  var name = `Nick's "Matei"`;
+  console.info("I'm employed", employed);
+  console.info(typeof employed);
+  console.warn("age", age, typeof age);
+  console.info("My name is", name, typeof name);
 
-age = 35;
-console.warn("age", age, typeof age);
+  age = 35;
+  console.warn("age", age, typeof age);
 
-age = false;
-console.warn("age", age, typeof age);
+  age = false;
+  console.warn("age", age, typeof age);
 
-var skills = ["HTML", "CSS", `JS`];
-console.debug(skills, typeof skills);
+  var skills = ["HTML", "CSS", `JS`];
+  console.debug(skills, typeof skills);
 
-var person = {
-  employed: true,
-  age: 29,
-};
-console.info(person, person.age, typeof person);
+  var person = {
+    employed: true,
+    age: 29,
+  };
+  console.info(person, person.age, typeof person);
+}
 
-var jobTitle = document.getElementById("job-title");
-console.warn(jobTitle, typeof jobTitle);
+function updateTitle(title) {
+  var jobTitle = document.getElementById("job-title");
+  console.warn(jobTitle, typeof jobTitle);
 
-console.info(jobTitle.innerHTML);
-jobTitle.innerHTML = "Trainer @ Udemy";
-console.info(jobTitle.innerHTML);
+  console.info(jobTitle.innerHTML);
+  jobTitle.innerHTML = title;
+  console.info(jobTitle.innerHTML);
+}
+
+variablesExample();
+
+updateTitle("Trainer @ Udemy");
