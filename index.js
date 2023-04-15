5ed4d8f74ab6d2c4785390dc6436832ab73574f2
Matei Nicolae <nmatei@sdl.com>
2023-04-15 21:36:48 +0300

    hide function, default home, move events to js

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
+function hide(id) {
+  document.getElementById(id).style.display = "none";
+}
+
 function showHomePage() {
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("projects").style.display = "none";
-  document.getElementById("languages").style.display = "none";
+  hide("skills");
+  hide("projects");
+  hide("languages");
   document.getElementById("home").style.display = "block";
 }
 
 function showSkillsPage() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("projects").style.display = "none";
-  document.getElementById("languages").style.display = "none";
+  hide("home");
+  hide("projects");
+  hide("languages");
   document.getElementById("skills").style.display = "block";
 }
 
 function showProjectsPage() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("languages").style.display = "none";
+  hide("home");
+  hide("skills");
+  hide("languages");
   document.getElementById("projects").style.display = "block";
 }
 
 function showLanguagesPage() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("projects").style.display = "none";
+  hide("home");
+  hide("skills");
+  hide("projects");
   document.getElementById("languages").style.display = "block";
 }
 
-showSkillsPage();
+showHomePage();
 
 var homeLink = document.querySelectorAll("#top-menu-bar a")[0];
 homeLink.addEventListener("click", showHomePage);
+
+var skillsLink = document.querySelectorAll("#top-menu-bar a")[1];
+skillsLink.addEventListener("click", showSkillsPage);
 
 var projectsLink = document.querySelectorAll("#top-menu-bar a")[2];
 projectsLink.addEventListener("click", showProjectsPage);
