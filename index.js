11897e3bb4571390345c024166966eb727413027
Matei Nicolae <nmatei@sdl.com>
2023-04-22 13:42:26 +0300

    improve events and add showPage(id)

--- index.js
+++ index.js
@@ -8,40 +8,16 @@ function show(id) {
   document.getElementById(id).style.display = "block";
 }
 
-function showHomePage() {
+function showPage(id) {
   hide(activePage);
-  show("home");
-  activePage = "home";
+  show(id);
+  activePage = id;
 }
 
-function showSkillsPage() {
-  hide(activePage);
-  show("skills");
-  activePage = "skills";
-}
+showPage(activePage);
 
-function showProjectsPage() {
-  hide(activePage);
-  show("projects");
-  activePage = "projects";
-}
-
-function showLanguagesPage() {
-  hide(activePage);
-  show("languages");
-  activePage = "languages";
-}
-
-showHomePage();
-
-var homeLink = document.querySelectorAll("#top-menu-bar a")[0];
-homeLink.addEventListener("click", showHomePage);
-
-var skillsLink = document.querySelectorAll("#top-menu-bar a")[1];
-skillsLink.addEventListener("click", showSkillsPage);
-
-var projectsLink = document.querySelectorAll("#top-menu-bar a")[2];
-projectsLink.addEventListener("click", showProjectsPage);
-
-var languagesLink = document.querySelectorAll("#top-menu-bar a")[3];
-languagesLink.addEventListener("click", showLanguagesPage);
+document.querySelector("#top-menu-bar").addEventListener("click", function (e) {
+  var id = e.target.dataset.page;
+  console.info("click on menu-bar", id);
+  showPage(id);
+});
