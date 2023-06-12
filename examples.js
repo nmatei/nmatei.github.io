2e6447afd00a0c6d514f69a907d091d88a679d27
Matei Nicolae <nmatei@sdl.com>
2023-06-12 20:50:11 +0300

    examples

---
+++ examples.js
@@ -0,0 +1,21 @@
+var employed = false; // boolean
+var age = 24; // number
+var name = "First Last"; // string
+
+console.info(typeof employed);
+console.info(typeof age);
+console.info(typeof name);
+
+employed = `Yes working @ "RWS"!, I'll still work there.`;
+console.info(typeof employed, employed);
+
+var skills = [1, "html", `css`, true];
+console.info(typeof skills, skills[1], skills.length);
+
+var person = {
+  name: "Matei",
+  age: 18,
+  employed: true,
+  skills: ["html", "css", "js"],
+};
+console.warn(typeof person, person);
