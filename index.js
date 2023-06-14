5bf071ceb6bb33d24d5a03f28db1bd586d9752d7
Matei Nicolae <nmatei@sdl.com>
2023-06-14 21:15:44 +0300

    add showHome and showSkills functions

---
+++ index.js
@@ -0,0 +1,11 @@
+function showHome() {
+  var page = document.getElementById("home");
+  page.style.display = "block";
+}
+
+function showSkills() {
+  var page = document.getElementById("skills");
+  page.style.display = "block";
+}
+
+showHome();
