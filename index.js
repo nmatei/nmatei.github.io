60c673626e7b8459ecd0b3bdb5bec06b24af2c96
Matei Nicolae <nmatei@sdl.com>
2023-06-15 18:18:45 +0300

    add hide and show functions

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+function hide(id) {
+  document.getElementById(id).style.display = "none";
+}
+function show(id) {
+  var page = document.getElementById(id);
+  page.style.display = "block";
+}
+
 function hideAllPages() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("languages").style.display = "none";
-  document.getElementById("projects").style.display = "none";
+  hide("home");
+  hide("skills");
+  hide("languages");
+  hide("projects");
 }
 
 function displayPage(id) {
   hideAllPages();
-  var page = document.getElementById(id);
-  page.style.display = "block";
+  show(id);
 }
 
 displayPage("home");
