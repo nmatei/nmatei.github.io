d03cfa22bf45c526280c94d5383121963550178c
Matei Nicolae <nmatei@sdl.com>
2023-06-16 20:55:33 +0300

    use activePage to store last visible page

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
+var activePage = "home";
+
+// utilities functions
+
 function hide(id) {
   console.info("hide %o element", id);
   document.getElementById(id).style.display = "none";
 }
 
-function hideAllPages() {
-  hide("home");
-  hide("skills");
-  hide("languages");
-  hide("projects");
+function showPage(id) {
+  hide(activePage);
+  var page = document.getElementById(id);
+  console.info("show %o", id, page);
+  page.style.display = "block";
+  activePage = id;
 }
 
-function showPage(id) {
-  hideAllPages();
-  var page = document.getElementById(id);
-  console.info("show", id, page);
-  page.style.display = "block";
-}
+// start our code
 
 showPage("home");
