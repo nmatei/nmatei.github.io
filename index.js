fa5660d6b2c5293cb003325ce69b24bed611d653
Matei Nicolae <nmatei@sdl.com>
2023-06-16 19:09:35 +0300

    hide all pages before showing other pages

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
+function hideAllPages() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+}
+
 function showHome() {
+  hideAllPages();
   var page = document.getElementById("home");
   page.style.display = "block";
 }
 
 function showSkills() {
+  hideAllPages();
   var page = document.getElementById("skills");
   page.style.display = "block";
 }
 
 function showLanguages() {
+  hideAllPages();
   var page = document.getElementById("languages");
   page.style.display = "block";
 }
 
 function showProjects() {
+  hideAllPages();
   var page = document.getElementById("projects");
   page.style.display = "block";
 }
