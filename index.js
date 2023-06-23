023a71e466133646a8d38b99549df1211a08e292
Matei Nicolae <nmatei@sdl.com>
2023-06-23 18:27:57 +0300

    showSkills from js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var activePage = "home";
+var activePage = "skills";
 
 // utilities functions
 
@@ -44,7 +44,18 @@ function clickOnMenu(e) {
   }
 }
 
+function showSkills() {
+  var skills = ["HTML", "css", "JS"];
+  var htmlSkills = skills.map(function (skill) {
+    // <li class="favorite">HTML</li>
+    return `<li>${skill}</li>`;
+  });
+  var ul = $("#skills ul");
+  ul.innerHTML = htmlSkills.join("");
+}
+
 // start our code
 
-showPage("home");
+showPage(activePage);
 $("#top-menu-bar").addEventListener("click", clickOnMenu);
+showSkills();
