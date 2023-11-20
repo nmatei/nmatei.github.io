c656bc59b6eea97798fd8c0265a3d18b1f8defb6
Matei Nicolae <nmatei@sdl.com>
2023-11-20 20:42:01 +0200

    extract js in new file

---
+++ examples.js
@@ -0,0 +1,17 @@
+console.info(2 + 3);
+console.warn("Nicolae Matei");
+
+var employed = true;
+var age = 18;
+console.log("Angajat:", employed);
+employed = false;
+console.log("Angajat:", employed);
+employed = "da";
+console.log("Angajat:", employed);
+
+var skills = ["html", "css"];
+console.debug("tipul variabilei skills:", typeof skills);
+
+var job = document.getElementById("job-title");
+console.warn("job", job);
+console.info(typeof job);
