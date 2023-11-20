24091e8f3210b0a6408a4a72b980a4a2d09c3817
Matei Nicolae <nmatei@sdl.com>
2023-11-20 20:59:31 +0200

    add function examples

--- examples.js
+++ examples.js
@@ -1,17 +1,21 @@
-console.info(2 + 3);
-console.warn("Nicolae Matei");
+function variablesExamples() {
+  console.info(2 + 3);
+  console.warn("Nicolae Matei");
 
-var employed = true;
-var age = 18;
-console.log("Angajat:", employed);
-employed = false;
-console.log("Angajat:", employed);
-employed = "da";
-console.log("Angajat:", employed);
+  var employed = true;
+  var age = 18;
+  console.log("Angajat:", employed);
+  employed = false;
+  console.log("Angajat:", employed);
+  employed = "da";
+  console.log("Angajat:", employed);
 
-var skills = ["html", "css"];
-console.debug("tipul variabilei skills:", typeof skills);
+  var skills = ["html", "css"];
+  console.debug("tipul variabilei skills:", typeof skills);
 
-var job = document.getElementById("job-title");
-console.warn("job", job);
-console.info(typeof job);
+  var job = document.getElementById("job-title");
+  console.warn("job", job);
+  console.info(typeof job);
+}
+
+variablesExamples();
