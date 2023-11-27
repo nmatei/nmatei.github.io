f4a6d567b23c45f2f382cef191295e6041842006
Matei Nicolae <nmatei@sdl.com>
2023-11-27 21:14:08 +0200

    hide old page

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function showLanguages() {
+  var oldPage = document.getElementById("projects");
+  oldPage.style.display = "none";
+
   var page = document.getElementById("languages");
   page.style.display = "block";
 }
