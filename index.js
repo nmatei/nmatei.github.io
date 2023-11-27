4b98f3d5809fe8a6daf4e84078b807d8125f4b72
Matei Nicolae <nmatei@sdl.com>
2023-11-27 21:25:12 +0200

    add show skills function

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function showSkills() {
+  var oldPage = document.getElementById("projects");
+  oldPage.style.display = "none";
+
+  var page = document.getElementById("skills");
+  page.style.display = "block";
+}
+
 function showLanguages() {
   var oldPage = document.getElementById("projects");
   oldPage.style.display = "none";
