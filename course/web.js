209f34e7206a661c251ed55b6846d5c3b318256f
Matei Nicolae <nmatei@sdl.com>
2023-12-12 23:21:47 +0200

    improve css for expired coupons

--- course/web.js
+++ course/web.js
@@ -8,7 +8,29 @@ function redirectToUdemy() {
   }
 }
 
+function checkExpired() {
+  // TODO if expired use referal code: DCED6F67EFF597AA11CE
+
+  // var now = new Date("2024-01-08T20:16:02.052Z").getTime();
+  var now = new Date("2024-01-07T20:16:02.052Z").getTime();
+  var first = 1;
+  document.querySelectorAll("#coupons li").forEach(function (li) {
+    var date = new Date(li.dataset.expire);
+    console.info("date", date);
+    if (date.getTime() < now) {
+      li.classList.add("expired");
+      li.title = "Expired: " + date.toDateString();
+      if (first) {
+        first = 0;
+        li.classList.add("first-expired");
+      }
+    }
+  });
+}
+
 (function () {
+  checkExpired();
+
   var redirectSec = 30;
   var secondsEl = document.querySelector("#redirect-info .seconds");
   var stopEl = document.querySelector("#redirect-info .btn-stop");
--- js/add-coupon.js
+++ js/add-coupon.js
@@ -14,8 +14,21 @@ const couponCode = args[1];
 const htmlContent = fs.readFileSync(htmlFilePath, "utf-8");
 const $ = cheerio.load(htmlContent);
 
-const url = `https://www.udemy.com/course/become-a-web-developer-from-scratch-step-by-step-guide/?couponCode=${couponCode}`;
-const newCoupon = $("<li>").html(`<a target="_blank" href="${url}">${couponCode}</a>`);
+function getCoupon(couponCode) {
+  var url = `https://www.udemy.com/course/become-a-web-developer-from-scratch-step-by-step-guide/?couponCode=${couponCode}`;
+
+  var link = `<a target="_blank" href="${url}">
+    <span class="coupon-code">${couponCode}</span>
+    <div class="coupon-info">
+      <span>Valid until:</span>
+      <span class="coupon-expire-date">Tue Oct 17 2023</span>
+    </div>
+  </a>`;
+
+  return $('<li data-expire="2023-10-17T20:16:02.052Z">').html(link);
+}
+
+const newCoupon = getCoupon(couponCode);
 
 $("#coupons ul").prepend(newCoupon);
 
