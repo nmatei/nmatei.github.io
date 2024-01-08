2e42c6b96f4a2f52fd20c1511dda78061d034490
Matei Nicolae <nmatei@sdl.com>
2024-01-08 21:40:33 +0200

    render skills from js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // variable publice
-var activePage = "home";
+var activePage = "skills";
 
 // functii publice
 function hide(id) {
@@ -37,6 +37,14 @@ function initEvents() {
   });
 }
 
+function showSkills() {
+  var ul = document.querySelector("#skills ul");
+  ul.innerHTML = "<li>HTML</li>";
+  ul.innerHTML = ul.innerHTML + "<li>CSS</li>";
+  ul.innerHTML += "<li>JS</li>";
+}
+
 // excutii
+showSkills();
 showPage(activePage);
 initEvents();
