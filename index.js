98b2f26d91e4be7b069f089b926445a5c3dce7b0
Matei Nicolae <nmatei@sdl.com>
2024-01-29 20:35:22 +0200

    use const and let instead of var

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 // variable publice
-var activePage = "skills";
+let activePage = "skills";
 
 // functii publice
 function $(selector) {
-  var el = document.querySelector(selector);
+  const el = document.querySelector(selector);
   // console.info("%o found:", selector, el);
   return el;
 }
 
 function hide(id) {
-  console.info("hide", id);
+  // console.info("hide", id);
   $("#" + id).style.display = "none";
 }
 
 function show(id) {
-  console.info("show", id);
-  var page = $(`#${id}`);
-  console.debug("show page", page);
+  // console.info("show", id);
+  const page = $(`#${id}`);
+  // console.debug("show page", page);
   page.style.display = "block";
 }
 
 function showPage(id) {
-  console.info("show page", id);
-  var prevLink = $("a[data-page=" + activePage + "]");
+  // console.info("show page", id);
+  const prevLink = $("a[data-page=" + activePage + "]");
   prevLink.classList.remove("active");
   hide(activePage);
 
-  var nextLink = $(`a[data-page=${id}]`);
+  const nextLink = $(`a[data-page=${id}]`);
   nextLink.classList.add("active");
   show(id);
   activePage = id;
 }
 
 function initEvents() {
-  var toolbar = $("#top-menu-bar");
+  const toolbar = $("#top-menu-bar");
   toolbar.addEventListener("click", function (e) {
     if (e.target.matches("a")) {
-      var page = e.target.dataset.page;
-      console.warn("click", page);
+      const page = e.target.dataset.page;
+      console.warn("click %o", page);
       showPage(page);
     }
   });
@@ -55,10 +55,10 @@ function sortByName(a, b) {
 function showSkills(skills) {
   //skills.sort(sortSkillsByEndorcements);
   skills.sort(sortByName);
-  var ul = $("#skills ul");
+  const ul = $("#skills ul");
 
-  var text = skills.map(function (skill) {
-    var cls = "";
+  const text = skills.map(function (skill) {
+    let cls = "";
     if (skill.favorite == true) {
       cls = "favorite";
     }
